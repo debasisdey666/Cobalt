dce7123a243424d5937eb9fba4ffe55d733c45e7
debasis <debasisdey666@gmail.com>
2024-02-26 12:31:45 +0530

    url2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // baseUrl: 'http://89.117.62.190/COBALT/'
-  // baseUrl: 'http://89.117.62.190/COBALT_DEV/',
+  baseUrl: 'http://89.117.62.190/COBALT_DEV/',
   // baseUrl: 'http://89.117.62.190/COBALT_DEMO/',
 };
