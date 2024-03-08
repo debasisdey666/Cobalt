372d75a2036c3749323557c2fc338c41dd8ab228
drifted-coder <ghosharunava57@gmail.com>
2024-03-08 12:21:46 +0530

    student-clearence

--- src/app/admin/student-clearence/student-clearence.component.ts
+++ src/app/admin/student-clearence/student-clearence.component.ts
@@ -20,39 +20,101 @@ export class StudentClearenceComponent implements OnInit {
   moreInfo: boolean = false;
   clearenceDataResponse: any;
   clearenceData: any
+  feesStatus!: string;
+  paperStatus!: string;
+  feesClearenceData: any;
+  paperClearenceData: any;
+  showPaymentDetails: boolean = false;
+  showPaperDetails: boolean = false;
+  feesClearenceModalHeader: string = "FEES CLEARENCE";
+  paperClearenceHeader: string = "PAPER CLEARENCE";
+  disableCheckBox: boolean = false;
 
   constructor(private clearenceService: StudentClearenceService) { }
 
   ngOnInit(): void {
   }
 
-  saveFormData(){
-    if(this.formData.regNumber != ''){
+  // getting the data 
+  getClearenceData(): void {
+    if (this.formData.regNumber != '') {
+      // Getting the data for fees clearence
+      this.clearenceService.getClearence(this.formData.regNumber, 1).subscribe((res: any) => {
+        this.feesClearenceData = res;
+
+        // Getting the data for paper clearence
+        this.clearenceService.getClearence(this.formData.regNumber, 2).subscribe((res: any) => {
+          this.paperClearenceData = res;
+          this.moreInfo = true;
+
+          // checking for fees clearence
+          if (this.feesClearenceData.Student_Payment_Clearance[0].PAYMENT_STATUS > 0) {
+            this.feesStatus = "YES"
+            this.showPaymentDetails = false;
+          }
+          else {
+            this.feesStatus = "NO";
+            this.showPaymentDetails = true;
+          }
+
+          // checking for paper clearence
+          if (this.paperClearenceData.Student_Paper_Clearance[0].PAPER_CLEARANCE > 0) {
+            this.paperStatus = "YES"
+            this.showPaperDetails = false;
+          }
+          else {
+            this.paperStatus = "NO";
+            this.showPaperDetails = true;
+          }
+
+          // calling API for checking checkbox
+          this.clearenceService.getClearenceStatus(this.formData.regNumber).subscribe((res: any) => {
+            if(res.Data.length == 0){
+              this.feesChecked = true
+              this.paperChecked = true
+              this.libraryChecked = true
+              this.disableCheckBox = true
+            }
+            else{
+              this.feesChecked = false
+              this.paperChecked = false
+              this.libraryChecked = false
+              this.disableCheckBox = false
+            }
+          })
+        })
+      })
+
+
+    }
+  }
+
+  saveFormData() {
+    if (this.formData.regNumber != '') {
       this.registrationNumber = this.formData.regNumber;
-      // calling the api
-      this.clearenceService.showClearence(this.registrationNumber).subscribe((res: any) => {
+      // calling the api to save data
+      this.clearenceService.saveClearence(this.registrationNumber).subscribe((res: any) => {
         this.clearenceDataResponse = res;
         this.clearenceData = this.clearenceDataResponse['Data'];
-        this.moreInfo = true;
       })
     }
   }
 
   onCheckboxChange(checkboxNumber: any) {
-    if(checkboxNumber == 1){
+    if (checkboxNumber == 1) {
       !this.feesChecked;
     }
-    if(checkboxNumber == 2){
+    if (checkboxNumber == 2) {
       !this.paperChecked;
     }
-    if(checkboxNumber == 3){
+    if (checkboxNumber == 3) {
       !this.libraryChecked;
     }
- 
-    if(this.feesChecked == true && this.paperChecked == true && this.libraryChecked == true){
+
+    if (this.feesChecked == true && this.paperChecked == true && this.libraryChecked == true) {
       this.isButtonEnabled = false;
     }
-    else{
+    else {
       this.isButtonEnabled = true;
     }
   }
--- src/app/services/student-clearence.service.ts
+++ src/app/services/student-clearence.service.ts
@@ -7,12 +7,36 @@ import { environment } from 'src/environments/environment';
 })
 export class StudentClearenceService {
 
-  url=environment.baseUrl+"api/AllClearance";
+  getUrl = environment.baseUrl+"api/MIS/checkAllClearance"
+  saveUrl=environment.baseUrl+"api/AllClearance";
 
   constructor(private http: HttpClient) { }
 
-  showClearence(regNumber: string){
-    const data = {
+  // Getting the data related to fees clearence & student clearence
+  getClearence(regNumber: string, reportType: number){
+    const payload = {
+      "studenT_REGISTRATION": regNumber,
+      "reporT_TYPE": reportType
+    }
+    return this.http.post(this.getUrl, payload);
+  }
+
+  // saving the data related to fees clearence & student clearence
+  saveClearence(regNumber: string){
+    const payload = {
+      "id": 0,
+      "studenT_REGISTRATION": regNumber,
+      "feeS_CLEARANCE": true,
+      "resulT_CLEARANCE": true,
+      "librarY_CLEARANCE": true,
+      "addedby": "1",
+      "mode": "U"
+    }
+    return this.http.post(this.saveUrl, payload);
+  }
+  // saving the data related to fees clearence & student clearence
+  getClearenceStatus(regNumber: string){
+    const payload = {
       "id": 0,
       "studenT_REGISTRATION": regNumber,
       "feeS_CLEARANCE": true,
@@ -21,6 +45,6 @@ export class StudentClearenceService {
       "addedby": "1",
       "mode": "V"
     }
-    return this.http.post(this.url, data);
+    return this.http.post(this.saveUrl, payload);
   }
 }
